b240e749c70a109275246157c4da58dedc6b579e
Peter van der Woude <pandawoude@gmail.com>
2008-07-23 20:28:39 -0700

    porting src

---
+++ Test.ExceptionReporter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using TestExceptionReporter;
+
+namespace Test.ExceptionReporter
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new TestExceptionReporterView());
+		}
+	}
+}
