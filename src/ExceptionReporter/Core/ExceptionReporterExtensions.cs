ef2475dbc11b4f292f3c2be17c551939856faf66
Peter van der Woude <pandawoude@gmail.com>
2009-02-26 15:55:06 -0800

    build xml comments by default

--- src/ExceptionReporter/Core/ExceptionReporterExtensions.cs
+++ src/ExceptionReporter/Core/ExceptionReporterExtensions.cs
@@ -1,7 +1,7 @@
 using System.Text;
 
-/// a hack (?) to ensure that we can use Extension Methods and still target .NET2
-/// see http://www.danielmoth.com/Blog/2007/05/using-extension-methods-in-fx-20.html
+// a hack (?) to ensure that we can use Extension Methods and still target .NET2
+// see http://www.danielmoth.com/Blog/2007/05/using-extension-methods-in-fx-20.html
 namespace System.Runtime.CompilerServices
 {
 	public class ExtensionAttribute : Attribute { }
