502cc60e87cefc636766d77effe668a5dd3fe461
Peter van der Woude <pandawoude@gmail.com>
2009-03-28 17:16:57 -0700

    partial commit

---
+++ src/ExceptionReporter/CoreProperties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("ExceptionReporter.Tests")]
+[assembly: AssemblyTitle("Exception Reporter Core")]
+[assembly: AssemblyDescription("Not used separately, merged with wither WPF or WinForms")]
+[assembly: AssemblyCompany("www.codeplex.com/ExceptionReporter")]
+[assembly: AssemblyProduct("Exception Reporter")]
+[assembly: AssemblyCopyright("Copyright © 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("63FFB39E-D5FC-4429-A416-241AB16A89F1")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
