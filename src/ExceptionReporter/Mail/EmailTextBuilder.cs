235e4736d81750878bbe15cbab86a3dc7b5e6f74
Peter van der Woude <pandawoude@gmail.com>
2009-09-03 23:41:18 -0700

    tidy up whitespace in email

--- src/ExceptionReporter/Mail/EmailTextBuilder.cs
+++ src/ExceptionReporter/Mail/EmailTextBuilder.cs
@@ -9,12 +9,13 @@ namespace ExceptionReporting.Mail
     {
         public string CreateIntro(bool takeScreenshot)
         {
-            var s = new StringBuilder("The email is ready to be sent.")
+            var s = new StringBuilder()
+                .AppendLine("The email is ready to be sent.")
                 .AppendLine("Information relating to the error is included. Please feel free to add any relevant information or attach any files.");
 
             if (takeScreenshot)
             {
-                s.AppendLine("A screenshot, taken at the time of the exception, is attached")
+                s.AppendLine("A screenshot, taken at the time of the exception, is attached.")
                  .AppendLine("You may delete the attachment before sending if you prefer.")
                  .AppendLine();
             }
