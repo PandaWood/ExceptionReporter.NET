003106a933fd71dbf041498a0a7cc0c33e43dbda
Peter van der Woude <pandawoude@gmail.com>
2011-02-01 22:46:30 -0800

    sign assemblies

--- src/ExceptionReporter/WinFormsProperties/AssemblyInfo.cs
+++ src/ExceptionReporter/WinFormsProperties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("ExceptionReporter.Tests")]
+[assembly: InternalsVisibleTo("ExceptionReporter.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009F8F4E3CC1172B9056781D8F82FEC519C577BC6756B690B1660D58BE30CBF6E5A6EFB5B925855DCAD8AAB9904EC3358577A5A221EAD082C16250423F8CD70D75223DB1B1A6C9F796B2EF57A87A71BFDE051BCEDE28156C1CB23C6F92043D0CA80ADB21F443971C464BE159E273B1913A98F70DD15334C80B7D11F2A20C9715AF")]
 [assembly: AssemblyTitle("ExceptionReporter.WinForms")]
 [assembly: AssemblyDescription("Displays exception details and system information")]
 [assembly: AssemblyConfiguration("")]
