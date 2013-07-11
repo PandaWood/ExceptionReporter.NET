42fc34da5a1a9c8ca7ec81d3a7fcf552334d2a8c
Peter van der Woude <pandawoude@gmail.com>
2013-07-11 13:19:51 +1000

    added (xml) documentation to exceptionreportinfo properties, with ref tags etc to make more information available to the programmer using coding tools eg VisualStudio tips or R# Ctrl-q

--- src/ExceptionReporter/Core/ExceptionReportInfo.cs
+++ src/ExceptionReporter/Core/ExceptionReportInfo.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Reflection;
+using System.Windows.Forms;
 
 #pragma warning disable 1591
 
@@ -16,7 +17,7 @@ namespace ExceptionReporting.Core
 		private readonly List<Exception> _exceptions = new List<Exception>();
 
 		/// <summary>
-		/// The Main (ostensibly 'only') exception, which is the subject of this exception 'report'
+		/// The Main (for the most part the 'only') exception, which is the subject of this exception 'report'
 		/// Setting this property will clear any previously set exceptions
 		/// <remarks>If multiple top-level exceptions are required, use SetExceptions instead</remarks>
 		/// </summary>
@@ -39,7 +40,7 @@ namespace ExceptionReporting.Core
 		/// Add multiple exceptions to be shown (each in a separate tab if shown in dialog)
 		/// <remarks>
 		/// Note: Showing multiple exceptions is a special-case requirement - for only 1 top-level exception
-		/// use the MainException property instead
+		/// use the <see cref="MainException"/> property instead
 		/// </remarks>
 		/// </summary>
 		public void SetExceptions(IEnumerable<Exception> exceptions)
@@ -55,21 +56,57 @@ namespace ExceptionReporting.Core
 		public string SmtpServer { get; set; }
 
 		/// <summary>
-		/// an email that is displayed in the 'Contact Information' (see EmailReportAddress for the email used to actually send)
+		/// Email that is displayed in the 'Contact Information'. See <see cref="EmailReportAddress"/> 
+		/// for the email actually used to send if using SimpleMAPI or SMTP)
 		/// </summary>
 		public string ContactEmail { get; set; }
 
+		/// <summary>
+		/// The name of the running application using <see cref="Application.ProductName"/>
+		/// </summary>
 		public string AppName { get; set; }
+
+		/// <summary>
+		/// The version of the running application using <see cref="Application.ProductVersion"/>
+		/// </summary>
 		public string AppVersion { get; set; }
+
+		/// <summary>
+		/// Region information using <see cref="Application.CurrentCulture.DisplayName"/>
+		/// </summary>
 		public string RegionInfo { get; set; }
+
+		/// <summary>
+		/// The name of the running application's machine using <see cref="Environment.MachineName"/>
+		/// </summary>
 		public string MachineName { get; set; }
+
+		/// <summary>
+		/// The name of the running application's current user. Defaults to <see cref="Environment.UserName"/>
+		/// </summary>
 		public string UserName { get; set; }
+
 		public DateTime ExceptionDate { get; set; }
+
+		/// <summary>
+		/// The text filled in by the user of the Exception Reporter dialog
+		/// </summary>
 		public string UserExplanation { get; set; }
+
+		/// <summary>
+		/// The calling assembly of the running application
+		/// If not set, will default to <see cref="Assembly.GetEntryAssembly()"/> ?? <see cref="Assembly.GetCallingAssembly()"/>
+		/// </summary>
 		public Assembly AppAssembly { get; set; }
+
 		public string WebUrl { get; set; }
 		public string Phone { get; set; }
 		public string Fax { get; set; }
+
+		/// <summary>
+		/// The company/owner of the running application.
+		/// Used in the dialog label that reads '...please contact {0} support'
+		/// </summary>
 		public string CompanyName { get; set; }
 
 		public bool ShowGeneralTab { get; set; }
@@ -80,31 +117,61 @@ namespace ExceptionReporting.Core
 		public bool ShowAssembliesTab { get; set; }
 
 		/// <summary>
-		/// address that is used to send an email (eg appears in the 'to:' field in the default email client if simpleMAPI)
+		/// Email address used to send an email (eg appears in the 'to:' field in the default email client if simpleMAPI)
 		/// </summary>
 		public string EmailReportAddress { get; set; }
 
+		/// <summary>
+		/// Default is <see cref="DefaultLabelMessages.DefaultExplanationLabel"/>
+		/// </summary>
 		public string UserExplanationLabel { get; set; }
+
 		public string ContactMessageTop { get; set; }
 
 		public bool ShowFlatButtons { get; set; }
 		public bool ShowLessMoreDetailButton{ get; set; }
 		public bool ShowFullDetail { get; set; }
 		public bool ShowButtonIcons { get; set; }
+
+		/// <summary>
+		/// Dialog title text
+		/// </summary>
 		public string TitleText { get; set; }
 
 		public Color BackgroundColor { get; set; }
 		public float UserExplanationFontSize { get; set; }
 
+		/// <summary>
+		/// Take a screenshot automatically at the point of calling <see cref="ExceptionReporter.Show(System.Exception[])"/>
+		/// which will then be available if sending an email using the ExceptionReporter dialog functionality
+		/// </summary>
 		public bool TakeScreenshot { get; set; }
+
+		/// <summary>
+		/// The Screenshot Bitmap, used internally
+		/// </summary>
 		public Bitmap ScreenshotImage { get; set; }
+
+		/// <summary>
+		/// Which email method to use (SMTP or SimpleMAPI) 
+		/// SimpleMAPI basically means it will try to use an installed Email client on the user's machine (eg Outlook)
+		/// </summary>
 		public EmailMethod MailMethod { get; set; }
 
+		/// <summary>
+		/// Whether a screenshot is configured to be taken and that it has been taken - used internally
+		/// </summary>
 		public bool ScreenshotAvailable 
 		{ 
 			get { return TakeScreenshot && ScreenshotImage != null; }
 		}
 
+		/// <summary>
+		/// Any files to attach to email (SMTP or SimpleMAPI) 
+		/// This is in addition to the automatically attached screenshot, if configured via TakeScreenshot
+		/// Any file attached needs to be a registered MIME type extension to work(?)
+		/// (attaching .tmp doesn't get attached in SimpleMAPI - testing using Outlook2013)
+		/// </summary>
 		public string[] FilesToAttach { get; set; }
 
 		public ExceptionReportInfo()
