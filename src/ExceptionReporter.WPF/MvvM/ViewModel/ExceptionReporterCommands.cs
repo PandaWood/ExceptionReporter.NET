a7a8664bdf554fef967928facdbb336015e6863d
Peter van der Woude <pandawoude@gmail.com>
2021-08-08 23:41:17 +1000

    tidy

--- src/ExceptionReporter.WPF/MvvM/ViewModel/ExceptionReporterCommands.cs
+++ src/ExceptionReporter.WPF/MvvM/ViewModel/ExceptionReporterCommands.cs
@@ -6,10 +6,12 @@ namespace ExceptionReporting.WPF.MvvM.ViewModel
 	public class ExceptionReporterCommands
 	{
 		public static readonly RoutedUICommand EmailCommand;
+		public static readonly RoutedUICommand ShowDetailsCommand;
 
 		static ExceptionReporterCommands()
 		{
 			EmailCommand = new RoutedUICommand("Send email", "EmailReport", typeof(ExceptionReporterCommands));
+			ShowDetailsCommand = new RoutedUICommand("Show details", "ShowDetails", typeof(ExceptionReporterCommands));
 		}
 	}
 }
